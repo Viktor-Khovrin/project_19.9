134a318ceda912c8ad9cd4f13362ed230f6fdd3f
Viktor-Khovrin <hovrin@mail.ru>
2023-04-19 11:11:13 +0300

    GitIgnore

--- app/src/main/java/com/example/FilmsSearch/MainActivity.kt
+++ app/src/main/java/com/example/FilmsSearch/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.FilmsSearch.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private var time_pressed = 0L
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
         initNavigation()
